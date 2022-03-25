4cdb2aeeada6c2e393e420dd9c22eeb5fb43cc6c
Marin Andreev <48124400+marinandreew@users.noreply.github.com>
2022-03-25 11:33:56 +0200

    add input/output

---
+++ CalculatorGIT/CalculatorGIT/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace CalculatorGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int output = 0;
+            while (input != "END")
+            {
+                string[] commandArgs = input.Split();
+                switch (commandArgs[0])
+                {
+                    case "+":
+                        output = Add(commandArgs.Skip(1).ToArray());
+                        break;
+                    case "-":
+                        output = Sub(commandArgs.Skip(1).ToArray());
+                        break;
+                    case "*":
+                        output = Mult(commandArgs.Skip(1).ToArray());
+                        break;
+                    case "/":
+                        output = Div(commandArgs.Skip(1).ToArray());
+                        break;
+                    case "POW":
+                        output = Pow(commandArgs.Skip(1).ToArray());
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
